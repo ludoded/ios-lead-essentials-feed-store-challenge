7887d236f699eebd36c52cba5c4c927ddc6ac4f5
Haik Ampardjian <haik.ampardjian@gmail.com>
2021-08-18 20:38:51 +0300

    Capture the context in the block, not externally by reassigning it to a local variable

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -73,7 +73,6 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	private func perform(action: @escaping (NSManagedObjectContext) -> Void) {
-		let context = self.context
-		context.perform { action(context) }
+		context.perform { [context] in action(context) }
 	}
 }
