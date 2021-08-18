dde12bad3c373e3b72b6bec831590fb30de36d53
Haik Ampardjian <haik.ampardjian@gmail.com>
2021-08-18 15:21:10 +0300

    Make sure that sut retrieves the values that were earlier inserted

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -29,11 +29,48 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		let context = self.context
+		context.perform {
+			let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+			request.returnsObjectsAsFaults = false
+			do {
+				if let cache = try context.fetch(request).first {
+					let localFeed = cache.feed.compactMap { object -> LocalFeedImage? in
+						if let feedImage = (object as? ManagedFeedImage) {
+							return LocalFeedImage(id: feedImage.id, description: feedImage.imageDescription, location: feedImage.location, url: feedImage.url)
+						} else {
+							return nil
+						}
+					}
+					let timestamp = cache.timestamp
+					completion(.found(feed: localFeed, timestamp: timestamp))
+				} else {
+					completion(.empty)
+				}
+			} catch {
+				completion(.failure(error))
+			}
+		}
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		fatalError("Must be implemented")
+		let context = self.context
+		context.perform {
+			let cache = ManagedCache(context: context)
+			cache.timestamp = timestamp
+			cache.feed = NSOrderedSet(array: feed.map({ localFeedImage in
+				let feedImage = ManagedFeedImage(context: context)
+				feedImage.id = localFeedImage.id
+				feedImage.imageDescription = localFeedImage.description
+				feedImage.location = localFeedImage.location
+				feedImage.url = localFeedImage.url
+
+				return feedImage
+			}))
+
+			try? context.save()
+			completion(nil)
+		}
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
