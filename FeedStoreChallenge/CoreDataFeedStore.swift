3bf70d69cfd271e9eb11fcf6595736f1d8257e90
Haik Ampardjian <haik.ampardjian@gmail.com>
2021-08-18 16:32:16 +0300

    Extracted the perform action into a separate private method

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -29,8 +29,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		let context = self.context
-		context.perform {
+		perform { context in
 			do {
 				if let cache = try ManagedCache.find(in: context) {
 					let localFeed = cache.localFeed
@@ -46,8 +45,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		let context = self.context
-		context.perform {
+		perform { context in
 			do {
 				try? ManagedCache.find(in: context).map(context.delete)
 
@@ -73,8 +71,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		let context = self.context
-		context.perform {
+		perform { context in
 			do {
 				try ManagedCache.find(in: context).map(context.delete)
 				try context.save()
@@ -84,5 +81,10 @@ public final class CoreDataFeedStore: FeedStore {
 				completion(error)
 			}
 		}
+	}
+
+	private func perform(action: @escaping (NSManagedObjectContext) -> Void) {
+		let context = self.context
+		context.perform { action(context) }
 	}
 }
